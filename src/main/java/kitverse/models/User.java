95a27c6402a133b7c2050b217eea28048f6a7e53
Aayush <aayushbastola772@gmail.com>
2026-04-21 23:49:44 +0545

    ADD: User attributes, getter/setter

--- src/main/java/kitverse/models/User.java
+++ src/main/java/kitverse/models/User.java
@@ -4,10 +4,89 @@
  */
 package kitverse.models;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author aayushbastola
  */
 public class User {
+    private int id;
+    private String fullName;
+    private String email;
+    private String phnNo;
+    private String password;
+    private String userType;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhnNo() {
+        return phnNo;
+    }
+
+    public void setPhnNo(String phnNo) {
+        this.phnNo = phnNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
     
 }
