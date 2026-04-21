ea8d2dd21591a0d78501178549c770f4c923b6e6
Aayush <aayushbastola772@gmail.com>
2026-04-21 23:50:23 +0545

    ADD: getUser and insertUser

--- src/main/java/kitverse/daoInterfaces/UserDAOInterface.java
+++ src/main/java/kitverse/daoInterfaces/UserDAOInterface.java
@@ -4,10 +4,17 @@
  */
 package kitverse.daoInterfaces;
 
+import kitverse.models.User;
+
 /**
  *
  * @author aayushbastola
  */
 public interface UserDAOInterface {
+     //create new user
+    int insertUser(String fullName,  String email, String phnNo, String password);
+    
+    //get password for the user
+    User getUser(String email);
     
 }
