072a6ee2e794e1f351217f92d7f7e5f8f3cd9989
Aayush <aayushbastola772@gmail.com>
2026-05-01 12:35:19 +0545

    ADD: product variant model

---
+++ src/main/java/kitverse/models/ProductVariant.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kitverse.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author aayushbastola
+ */
+public class ProductVariant {
+    private int variantId;
+    private int productId;
+    private String size;
+    private float sellingPrice;
+    private int stock;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+    public ProductVariant() {
+    }
+
+    public int getVariantId() {
+        return variantId;
+    }
+
+    public void setVariantId(int variantId) {
+        this.variantId = variantId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(float sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+    
+}
