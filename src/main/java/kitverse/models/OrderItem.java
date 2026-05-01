ba659898678f3331ecec614f6bb809a37d9c2065
Aayush <aayushbastola772@gmail.com>
2026-05-01 13:04:52 +0545

    ADD: orderItem model

---
+++ src/main/java/kitverse/models/OrderItem.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kitverse.models;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author aayushbastola
+ */
+public class OrderItem {
+    private int itemId;
+    private int orderId;
+    private int ProductVariantId;
+    private int quantity;
+    private String playerName;
+    private int playerNo;
+    //private double totalPrice;
+    private LocalDateTime createAt;
+    private LocalDateTime updatedAt;
+
+    public OrderItem() {
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductVariantId() {
+        return ProductVariantId;
+    }
+
+    public void setProductVariantId(int ProductVariantId) {
+        this.ProductVariantId = ProductVariantId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public int getPlayerNo() {
+        return playerNo;
+    }
+
+    public void setPlayerNo(int playerNo) {
+        this.playerNo = playerNo;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+}
