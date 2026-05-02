82b56ba918f159fe80998bb8a40a3b18e7816c12
Aayush <aayushbastola772@gmail.com>
2026-05-02 17:46:47 +0545

    ADD: invalidate session

---
+++ src/main/java/kitverse/servlets/LogoutServlet.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package kitverse.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import kitverse.utilities.SessionUtil;
+
+/**
+ *
+ * @author aayushbastola
+ */
+@WebServlet(name = "LogoutServlet", urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Invalidate the session that delete the user data from session 
+        SessionUtil.invalidateSession(request);
+         
+        // Now route to login page
+        response.sendRedirect("login");
+    }
+
+}
